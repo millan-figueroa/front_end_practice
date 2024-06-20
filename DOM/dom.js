8b3c7ce8fdc2ca4d1d242caece5a93bdeaccc3dd
millan figueroa <millan.fig@gmail.com>
2024-06-19 20:19:01 -0700

    Added more event listener exercises

--- DOM/dom.js
+++ DOM/dom.js
@@ -107,64 +107,118 @@
 
 
 
+
 // ***** TRAVERSING THE DOM *****
 
 
 // Query Select and save items list to variable and console log 
-let itemList = document.querySelector('#items');
+
 
 // Change the color of the parent node of items list. 
-itemList.parentNode.style.backgroundColor = 'grey';
+
+
 
 // Log the great great grandparent node of the items list to the console. parentNode & parentElement are interchangeable --> <body></body>
-console.log(itemList.parentElement.parentNode.parentNode);
 
-// Log the childNodes, then the children of the itemList. Which is better? 
-console.log(itemList.childNodes);
-console.log(itemList.children);
+
+
+// Log the child nodes, then the children of the itemList. Which is easier to work with?
+
+
 
 // Try turning the last child of items list yellow. If it doesn't work, log the last child. Notice we have the same issue as we did with childNodes? Now try doing it using lastElementChild. 
 
-console.log(itemList.lastElementChild);
-itemList.lastElementChild.style.backgroundColor = 'yellow';
+
 
 // Change the text of the last child to 'Special Item'
 
-itemList.lastElementChild.textContent = 'Special Item';
 
-// log the next sibling of the items list. What happens when you use nextElementSibling? --> null
 
-console.log(itemList.nextElementSibling);
+// Get the sibling so it logs the following element into the console --> <br>
 
-// Change the color of the previous sibling of the items list to pink. 
 
-itemList.previousElementSibling.style.backgroundColor = 'pink';
+
+// Change the color of the previous sibling of the items list to grey --> 'Items' header shld be grey
+
+
+
 
 
 // ***** CREATING ELEMENTS *****
 
-// Create a new div with a class and IDof hello
-let newDiv = document.createElement('div');
-newDiv.className = 'hello';
-newDiv.id = 'hello';
+// Create a new div with a class and ID of hello
+
+
 
 // Set a title of 'hello Div' to the new div
-newDiv.setAttribute('title', 'newDiv');
+
 
 
 // Create a new text node to the div that says 'Hello World!', then add that text to the div
-let newDivText = document.createTextNode('Hello World!');
-newDiv.appendChild(newDivText);
-console.log(newDiv);
 
-// Now add your newly created div to the DOM!
 
-let container = document.querySelector('header .container');
-let h1 = document.querySelector('header h1');
 
-container.insertBefore(newDiv,h1);
+// Now add your newly created div to the DOM! Place the new div inside the .container div, underneath the 'Items' h2.
 
-// Lastly, change the font size of the newDiv's text to 30px;
 
-newDiv.style.fontSize = '30px';
-console.log(newDiv);
+// Lastly, change the font size of the newDiv's text to 30px
+
+
+
+// ***** ADDING EVENT LISTENERS FOR INTERACTIVTY *****
+
+// Create a new button in the html, call it 'Make me Beautiful!', link an event to it by grabbing its id and assigning it to a variable, then adding an event listener and passing in a 'click' event, and a 'prettyMaker' func
+
+let button = document.getElementById('button');button.addEventListener('click', buttonClick);
+
+
+// Create a function prettyMaker that changes the 'add Items' container's background to pink when clicked 
+// let header = document.getElementById('main-header');
+// let items = document.getElementById('main');
+// let body = items.parentNode.parentNode;
+// console.log(body);
+// function prettyMaker() {
+//     header.className = 'bg-secondary text-white p-4 mb-3';
+//     items.style.backgroundColor = 'pink';
+//     button.textContent = 'Thank you!';
+//     body.style.backgroundColor = 'black';
+// }
+
+// Create a function buttonClick and pass in an even parameter and click button to see its props!
+
+// Add .target to param to see the element its targeting. Add target.id, target.className, target.classList, etc to see all the elements targeted 
+
+// Add .clientX or .clientY to see the position of the mouse on the appropriate axis, or .offsetX / offsetY to see the axis pertaining to the element
+
+function buttonClick(e) {
+    console.log(e.clientY);
+}
+// Add .altKey, .ctrlKey, .shiftKey and hold any of them down to see which boolean you get
+
+// Create a function runEvents and test out some events such as dblclick, mousedown, mouseup for the first param, and runEvent for the 2nd
+
+// Create an empty div in the html and create a function to test 'mouseenter','mouseleave', 'mouseover', and 'mouseout' events
+
+// Test focus / blur (click in, click out), cut/ paste (make everything disappear if you cut or paste), input 
+
+// Create drop down menu and add event listener to listen for selection ('change event'), console.log the chose value
+
+// Add event listener to forms and listen for submit, use preventDefault
+// let box = document.getElementById('box');
+// box.addEventListener('mousemove', runEvent);
+
+let itemInput = document.querySelector('input[type="text"]');
+let form = document.querySelector('form');
+
+itemInput.addEventListener('keydown', runEvent);
+function runEvent(e) {
+    console.log('EVENT TYPE: ' + e.type);
+    console.log(e.target.value);
+    document.getElementById('output').innerHTML = '<h3>'+e.target.value+'</h3';
+    // output.innerHTML = '<h3>MouseX: '+e.offsetX+' </h3><h3>MouseY: '+e.offsetY+'</h3';
+
+    // document.body.style.backgroundColor = "rgb("+e.offsetX+","+e.offsetY+",40)";
+}
+
+
+
